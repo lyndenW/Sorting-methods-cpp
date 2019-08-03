e523e18200237bb14dda0f4784908fc6d00307ad
Tianquan Wang <tianquan@umich.edu>
2019-08-03 17:07:02 +0800

    Update bubble_sort.h
    Explain how bubble sort can be adaptive

--- bubble_sort.h
+++ bubble_sort.h
@@ -4,3 +4,18 @@ void bubble(item a[], intleft, intright) {
             if (a[j] < a[j - 1])
                 swap(a[j - 1], a[j]);
 }
+
+// Adaptive version, stop early if already sorted.
+void bubble_ad(item a[], intleft, intright) {
+    for (int i = left; i < right - 1; ++i) {
+        bool swapped = false;
+        for (int j = right - 1; j > i; --j) {
+            if (a[j] < a[j - 1]) {
+                swapped = true;
+                swap(a[j - 1], a[j]);
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
